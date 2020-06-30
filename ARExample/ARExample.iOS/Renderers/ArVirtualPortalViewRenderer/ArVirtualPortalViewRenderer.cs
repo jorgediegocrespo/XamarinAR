77a8bc48ad5745194d2362c55712b250b1082590
Jorge DevsDNA <jorge@devsdna.com>
2020-06-30 07:10:21 +0200

    Portal images

--- ARExample/ARExample.iOS/Renderers/ArVirtualPortalViewRenderer/ArVirtualPortalViewRenderer.cs
+++ ARExample/ARExample.iOS/Renderers/ArVirtualPortalViewRenderer/ArVirtualPortalViewRenderer.cs
@@ -58,7 +58,7 @@ namespace ARExample.iOS.Renderers
             sceneView.DebugOptions = ARSCNDebugOptions.ShowFeaturePoints | ARSCNDebugOptions.ShowWorldOrigin;
             config.PlaneDetection = ARPlaneDetection.Horizontal;
             sceneView.Session.Run(config, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
-            sceneView.Delegate = new ArPlaneScnViewDelegate(sceneView);
+            sceneView.Delegate = new ArVirtualPortalScnViewDelegate(this);
 
             //Permite añadir reflejos a los objetos de la escena
             sceneView.AutoenablesDefaultLighting = true;
@@ -81,6 +81,9 @@ namespace ARExample.iOS.Renderers
             if (tapScene == null)
                 return;
 
+            if (!PlaneDetected)
+                return;
+
             CoreGraphics.CGPoint tapLocation = sender.LocationInView(tapScene);
             ARHitTestResult[] hitTest = tapScene.HitTest(tapLocation, ARHitTestResultType.ExistingPlaneUsingExtent);
             if (hitTest?.Any() != true)
@@ -91,38 +94,43 @@ namespace ARExample.iOS.Renderers
 
         private void ShowPortal(ARHitTestResult hitTestResult)
         {
-            //let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
-            //let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
-            //let transform = hitTestResult.worldTransform
-            //let planeXposition = transform.columns.3.x
-            //let planeYposition = transform.columns.3.y
-            //let planeZposition = transform.columns.3.z
-            //portalNode.position = SCNVector3(planeXposition, planeYposition, planeZposition)
-            //self.sceneView.scene.rootNode.addChildNode(portalNode)
-            //self.addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
-            //self.addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
-            //self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "back")
-            //self.addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideA")
-            //self.addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideB")
-            //self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideDoorA")
-            //self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideDoorB")
+            SCNScene portalScene = SCNScene.FromFile("art.scnassets/Portal.scn");
+            SCNNode portalNode = portalScene.RootNode.FindChildNode("Portal", false);
+            OpenTK.NMatrix4 transform = hitTestResult.WorldTransform;
+
+            float planeXposition = transform.Column3.X;
+            float planeYposition = transform.Column3.Y;
+            float planeZposition = transform.Column3.Z;
+
+            portalNode.Position = new SCNVector3(planeXposition, planeYposition, planeZposition);
+            sceneView.Scene.RootNode.AddChildNode(portalNode);
+
+            AddPlane("roof", portalNode, "portalTop.png");
+            AddPlane("floor", portalNode, "portalBottom.png");
+            AddWalls("backWall", portalNode, "portalBack.png");
+            AddWalls("sideWallA", portalNode, "portalSideA.png");
+            AddWalls("sideWallB", portalNode, "portalSideB.png");
+            AddWalls("sideDoorA", portalNode, "portalSideDoorA.png");
+            AddWalls("sideDoorB", portalNode, "portalSideDoorB.png");
         }
 
         private void AddWalls(string nodeName, SCNNode portalNode, string imageName)
         {
-            //let child = portalNode.childNode(withName: nodeName, recursively: true)
-            //child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
-            //child?.renderingOrder = 200
-            //if let mask = child?.childNode(withName: "mask", recursively: false) {
-            //        mask.geometry?.firstMaterial?.transparency = 0.000001
-            //}
+            SCNNode child = portalNode.FindChildNode(nodeName, true);
+            child.Geometry.FirstMaterial.Diffuse.Contents = new UIImage(imageName);
+            child.RenderingOrder = 200;
+            SCNNode mask = child.FindChildNode("mask", false);
+            if (mask != null)
+            {
+                mask.Geometry.FirstMaterial.Transparency = 0.000001f;
+            }
         }
 
         private void AddPlane(string nodeName, SCNNode portalNode, string imageName)
         {
-            //let child = portalNode.childNode(withName: nodeName, recursively: true)
-            //child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
-            //child?.renderingOrder = 200
+            SCNNode child = portalNode.FindChildNode(nodeName, true);
+            child.Geometry.FirstMaterial.Diffuse.Contents = new UIImage(imageName);
+            child.RenderingOrder = 200;
         }
     }
 }
